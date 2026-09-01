4629b9721c76d6b6021ae7505e9ddc4ae4520b54
Taufiq <tetopik@gmail.com>
2026-09-01 23:01:52 +0700

    enable esp32c3 CAN pheriperal

--- ports/esp32/boards/ESP32_GENERIC_C3/mpconfigboard.h
+++ ports/esp32/boards/ESP32_GENERIC_C3/mpconfigboard.h
@@ -1,9 +1,11 @@
 // This configuration is for a generic ESP32C3 board with 4MiB (or more) of flash.
 
 #ifndef MICROPY_HW_BOARD_NAME
-#define MICROPY_HW_BOARD_NAME               "ESP32C3 module"
+#define MICROPY_HW_BOARD_NAME               "ESP32C3 CAN Module"
 #endif
 #define MICROPY_HW_MCU_NAME                 "ESP32-C3"
 
-// Enable UART REPL for modules that have an external USB-UART and don't use native USB.
-#define MICROPY_HW_ENABLE_UART_REPL         (1)
+#define MICROPY_HW_ENABLE_UART_REPL  (0)
+#define MICROPY_HW_ENABLE_CAN        (1)
+#define MICROPY_HW_CAN1_TX          (21)
+#define MICROPY_HW_CAN1_RX          (20)
